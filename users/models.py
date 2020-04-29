9f95eb7d40ea3f51dd3d113d53a7141dded92ab4
adegbayi-adeosun25 <B00100241@student.itb.ie>
2020-04-29 22:34:20 +0100

    Updated req

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 
-import stripe
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
